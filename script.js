24f530e7a44552a1c6eaf214b9cc05f2c4210e20
theresao <theresaolloh@gmail.com>
2023-01-24 14:04:32 +0000

    hour blocks added

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $("currentDay").text(moment().format("DD MM YYYY, h:mm:ss a"));
+
+    $(".saveBTN").on("click", function () {
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+    }
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+}
+
